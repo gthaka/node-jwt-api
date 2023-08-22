6877bb8eeb80861bc6c4beaf968f6e9f546acd57
Anthony G <agithaka@live.com>
2023-08-22 11:29:40 +0300

    user model

---
+++ src/models/User.ts
@@ -0,0 +1,12 @@
+interface User {
+    id: string;
+    username: string;
+    email: string;
+    password: string;
+    is_verified: boolean;
+    emailVerificationToken: string | null
+    resetToken: string | null;
+    resetTokenCreatedAt: Date | null;
+}
+
+export default User;
