42d0916ecf95c7617b269ea41c026b79f92aeb8c
Anthony G <agithaka@live.com>
2023-08-23 06:53:16 +0300

    swagger conf routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,23 +1,239 @@
 import { Router } from 'express';
-import { authMiddleware, emailVerificationMiddleware } from '../middleware/authMiddleware'
+import { authMiddleware } from '../middleware/authMiddleware'
 import authController from '../controllers/authController';
-import { getUserDetails } from '../controllers/userController';
 
 const router = Router();
 
-// Signup and signin endpoints
+/**
+ * @swagger
+ * /auth/signup:
+ *   post:
+ *     summary: User sign-up
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Signup successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 token:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
+
 router.post('/signup', authController.signup);
+
+/**
+ * @swagger
+ * /auth/signin:
+ *   post:
+ *     summary: User sign-in
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Signin successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 token:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 router.post('/signin', authMiddleware, authController.signin);
 
-// Password reset endpoints
+/**
+ * @swagger
+ * /auth/forgot-password:
+ *   post:
+ *     summary: Initiate password reset
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Reset token generated and sent
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 router.post('/forgot-password', authController.forgotPassword);
+
+/**
+ * @swagger
+ * /auth/reset-password/{token}:
+ *   post:
+ *     summary: Reset password using token
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               newPassword:
+ *                 type: string
+ *     parameters:
+ *       - name: token
+ *         in: path
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Password reset successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 router.post('/reset-password/:token', authController.resetPassword);
 
-// Email verification endpoint
+/**
+ * @swagger
+ * /auth/verify-email/{token}:
+ *   get:
+ *     summary: Verify user email using token
+ *     tags:
+ *       - Authentication
+ *     parameters:
+ *       - name: token
+ *         in: path
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Email verification successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 router.get('/verify-email/:token', authController.verifyEmail);
-router.post('/resend-verification-email', authController.resendVerificationEmail);
 
-// Protected user details endpoint
-router.get('/user-details', authMiddleware, emailVerificationMiddleware, getUserDetails);
+/**
+ * @swagger
+ * /auth/resend-verification-email:
+ *   post:
+ *     summary: Resend email verification token
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Verification token resent
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
+router.post('/resend-verification-email', authController.resendVerificationEmail);
 
 export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,7 +4,41 @@ import { authMiddleware, emailVerificationMiddleware } from '../middleware/authM
 
 const router = Router();
 
-// Protected route - requires authentication
+/**
+ * @swagger
+ * /users/details:
+ *   get:
+ *     summary: Get user details
+ *     tags:
+ *       - User
+ *     security:
+ *       - BearerAuth: []  # Bearer token required
+ *     responses:
+ *       200:
+ *         description: User details fetched successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 username:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       403:
+ *         description: Forbidden
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
+
 router.get('/details', authMiddleware, emailVerificationMiddleware, getUserDetails);
 
 export default router;
